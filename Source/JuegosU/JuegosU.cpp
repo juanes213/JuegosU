82ea4cb1c1a7d8a9b178bcd0bbe0546507da7d6d
Juanes200 <juanes.200.200@gmail.com>
2022-02-04 13:38:02 -0500

    ** Creando el proyecto en Unreal. **

---
+++ Source/JuegosU/JuegosU.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "JuegosU.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JuegosU, "JuegosU" );
